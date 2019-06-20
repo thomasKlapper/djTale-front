ab096356ac9fa758c68228c8d6943522a0afbe4c
Jayet Sonny <sonny.jayet@gfi.fr>
2019-06-20 10:15:30 +0200

    fixed

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,12 @@ import { RegisterComponent } from './register/register.component';
 import { AuthService } from './auth.service';
 import { AuthRoutingModule } from './auth-routing.module';
 import { JwtModule } from '@auth0/angular-jwt';
+import { AppMaterialModule } from '../app-material/app-material.module';
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpLoaderFactory } from '../app.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 export function getJwt() {
     return window.localStorage.getItem('jwt');
@@ -12,6 +18,15 @@ export function getJwt() {
 
 @NgModule({
     imports: [
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      }),
+      ReactiveFormsModule,
+      AppMaterialModule,
         CommonModule,
         AuthRoutingModule,
         JwtModule.forRoot({
